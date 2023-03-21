d71ff7334e5890b8f80dcc7bb936f18cd597bbe5
ayushsachan123 <ayushsachan.in@gmail.com>
2023-03-21 22:29:42 +0530

    add colouring_Rectangle.cpp

---
+++ PROBLEMSET_Rating_Greater_than_1000/colouring_Rectangle.cpp
@@ -0,0 +1,20 @@
+//https: // codeforces.com/problemset/problem/1584/B
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n,m;
+    cin>>n>>m;
+    int ans = ceil((n*m)/3.0);
+    cout<<ans<<endl;
+}
+
+int main(){
+int t;
+cin>>t;
+while(t-->0){
+solve();
+}
+    return 0;
+}
