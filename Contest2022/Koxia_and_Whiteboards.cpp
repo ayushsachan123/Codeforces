9d55cdf7ccbbe847ea92ef4ec21101db79d572ed
ayushsachan123 <ayushsachan.in@gmail.com>
2023-03-25 21:38:12 +0530

    add Koxia_and_Whiteboards.cpp

---
+++ Contest2022/Koxia_and_Whiteboards.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int            long long int
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define endl           "\n"
+#define sz(x)          ((int) x.size())
+#define all(p)         p.begin(), p.end()
+#define double         long double
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << " " << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
+
+inline int power(int a, int b)
+{
+	int x = 1;
+	while (b)
+	{
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+void solve() {
+	int n,m;
+	cin>>n>>m;
+
+       vector<int> v1(n);
+
+		vector<int>v2(m);
+     for(int i=0;i<n;i++){
+          cin>>v1[i];
+        }
+        for(int i=0;i<m;i++){
+            cin>>v2[i];
+        }
+        int te =m,temp=0;
+
+        while(te--){
+            int minim=0;
+            for(int i=0;i<n;i++){
+         if(v1[i]<v1[minim]){
+                minim=i;
+                }
+            }
+
+            v1[minim]=v2[temp++];
+        }
+
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=v1[i];
+        }
+        cout<<sum<<endl;
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt",  "r",  stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+
+	int t = 1;
+        cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
